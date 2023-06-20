63d0df072d3169db773db8784a4a73aeef3895db
Chandan Goswami <goswami75@gmail.com>
2023-06-20 10:04:34 +0530

    Altered Docker files to adapt to Integra Tool

--- my-kafka/build-images.sh
+++ my-kafka/build-images.sh
@@ -2,7 +2,6 @@
 
 set -e
 
-docker build -t integra/base:latest  docker/base
+docker build -t integra/kafka-base:latest  docker/base
 docker build -t integra/kafka-zookeeper:latest  docker/zookeeper
 docker build -t integra/kafka-broker:latest  docker/broker
-#docker build -t Integra/kafka-client:latest  docker/client
--- my-spark/build-images.sh
+++ my-spark/build-images.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-docker build -t spark-base:latest ./docker/base
-docker build -t spark-master:latest ./docker/spark-master
-docker build -t spark-worker:latest ./docker/spark-worker
-docker build -t spark-submit:latest ./docker/spark-submit
+docker build -t integra/spark-base:latest ./docker/base
+docker build -t integra/spark-master:latest ./docker/spark-master
+docker build -t integra/spark-worker:latest ./docker/spark-worker
+docker build -t integra/spark-submit:latest ./docker/spark-submit
