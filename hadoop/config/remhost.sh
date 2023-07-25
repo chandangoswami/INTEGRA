7db1c8ef7d848c8a7d58535004c8d617f60a55da
Chandan Goswami <goswami75@gmail.com>
2023-07-25 12:52:50 +0530

    Following changes 1. Added config dir to have all the spark level config files. 2. Created only on Dockerfile Dockerfile_withHadoop_and_spark

--- hadoop/config/remhost.sh
+++ hadoop/config/remhost.sh
@@ -1,1 +1,5 @@
- for hst in `cat workers`; do remIp=$(ssh hadoop@$hst ifconfig | grep inet | grep -v 127| tr -s ' '| cut -d ' ' -f3) ; echo $remIp $hst >> host ; done 
+hst=hadoop-nameNode-1
+remIp=$(ssh hadoop@$hst ifconfig | grep inet | grep -v 127| tr -s ' '| cut -d ' ' -f3) ;
+echo $remIp $hst >> host 
+
+for hst in `cat workers`; do remIp=$(ssh hadoop@$hst ifconfig | grep inet | grep -v 127| tr -s ' '| cut -d ' ' -f3) ; echo $remIp $hst >> host ; done 
